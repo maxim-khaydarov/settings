62c0c05f56733047063c23bbe9dfb4fa74b916f8
Haydarov-M <Haydarov-M@NBKR3FTD0001.mc.gcf>
2016-01-02 19:29:10 +0200

    02/01/15 19:29

--- Settings/src/ua/mkh/settings/full/ActivityRegion.java
+++ Settings/src/ua/mkh/settings/full/ActivityRegion.java
@@ -76,6 +76,7 @@ public class ActivityRegion extends Activity implements OnClickListener, SimpleG
 			detector = new SimpleGestureFilter(this,this);
 			
 			textStatus.setTypeface(typefaceBold);
+			textStatus.setText(R.string.language_region);
 			btn_back.setTypeface(typefaceMedium);
 			Button01.setTypeface(typefaceRoman);
 			Button02.setTypeface(typefaceRoman);
@@ -133,6 +134,27 @@ public class ActivityRegion extends Activity implements OnClickListener, SimpleG
 		      
 		        get_lang();
 		        
+		        int speed = mSettings.getInt(APP_PREFERENCES_ANIM_SPEED, 1);
+				if (speed == 1){
+					center_to_right = R.anim.slide_center_to_right_short;
+					center_to_right2 = R.anim.slide_center_to_right2_short;
+					center_to_left = R.anim.slide_center_to_left_short;
+					center_to_left2 = R.anim.slide_center_to_left2_short;
+				}
+				if (speed == 2){
+					center_to_right = R.anim.slide_center_to_right_medium;
+					center_to_right2 = R.anim.slide_center_to_right2_medium;
+					center_to_left = R.anim.slide_center_to_left_medium;
+					center_to_left2 = R.anim.slide_center_to_left2_medium;
+				}
+				if (speed == 3){
+					center_to_right = R.anim.slide_center_to_right_long;
+					center_to_right2 = R.anim.slide_center_to_right2_long;
+					center_to_left = R.anim.slide_center_to_left_long;
+					center_to_left2 = R.anim.slide_center_to_left2_long;
+				}
+				
+		        
 		       if (mSettings.contains(APP_PREFERENCES_tgb_menu)) {
 					// Получаем число из настроек
 		        	 Boolean menu = mSettings.getBoolean(APP_PREFERENCES_tgb_menu, true);
@@ -168,33 +190,11 @@ public class ActivityRegion extends Activity implements OnClickListener, SimpleG
 						
 					}
 		    }
-		       
-		      
-					// Получаем число из настроек
-		        	 int speed = mSettings.getInt(APP_PREFERENCES_ANIM_SPEED, 1);
-					if (speed == 1){
-						center_to_right = R.anim.slide_center_to_right_short;
-						center_to_right2 = R.anim.slide_center_to_right2_short;
-						center_to_left = R.anim.slide_center_to_left_short;
-						center_to_left2 = R.anim.slide_center_to_left2_short;
-					}
-					if (speed == 2){
-						center_to_right = R.anim.slide_center_to_right_medium;
-						center_to_right2 = R.anim.slide_center_to_right2_medium;
-						center_to_left = R.anim.slide_center_to_left_medium;
-						center_to_left2 = R.anim.slide_center_to_left2_medium;
-					}
-					if (speed == 3){
-						center_to_right = R.anim.slide_center_to_right_long;
-						center_to_right2 = R.anim.slide_center_to_right2_long;
-						center_to_left = R.anim.slide_center_to_left_long;
-						center_to_left2 = R.anim.slide_center_to_left2_long;
-					}
-					
-		    
+		       }
 		 }
+					
 		
-}
+
 		 
 		 private void get_lang() {
 			
@@ -244,7 +244,7 @@ public class ActivityRegion extends Activity implements OnClickListener, SimpleG
 		 
 		 public void BackClick(View v)  
 		    {  
-		    	Intent intent18 = new Intent(this, MainActivity.class);
+		    	Intent intent18 = new Intent(this, ActivityOsnova.class);
 		     	 startActivity(intent18);
 
 				overridePendingTransition(center_to_right, center_to_right2);
@@ -263,7 +263,7 @@ public class ActivityRegion extends Activity implements OnClickListener, SimpleG
 		           	 }
 		                return true;
 		            case KeyEvent.KEYCODE_BACK:
-		            	Intent intent18 = new Intent(this, MainActivity.class);
+		            	Intent intent18 = new Intent(this, ActivityOsnova.class);
 		   	       	 startActivity(intent18);
 		   	        	overridePendingTransition(center_to_right, center_to_right2);
 		                return true;
